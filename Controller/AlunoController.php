12d048169b7c466298f3ee3c9749627ca3587c04
231284 <231284@CPD03-M07>
2025-04-14 14:56:54 -0300

    FINAlIZAÇÃO

--- Controller/AlunoController.php
+++ Controller/AlunoController.php
@@ -57,5 +57,22 @@ final class AlunoController extends Controller
             }
 
             public static function delete() : void 
-}
+            {
+                parent::isProtected();
+
+                $model = new Aluno();
+
+                try
+                {
+                    $model->delete( (int) $_GET['id']);
+                    parent::redirect("/aluno");
+
+                } catch(Exception $e) {
+                    $model->setError("Ocorreu um erro ao exluir o aluno:");
+                    $model->setError($e->getMessage());
+                }
+
+                parent::render('Aluno/lista_aluno.php', $model);
+                }
+            }//Fim da classe
 ?>
